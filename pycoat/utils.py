bf89d2c5ea9c74f62ce4f112bc386944e84db7cd
Maxim Stolyarchuk <mstolyarchuk@yahoo.com>
2013-09-27 21:11:47 +0400

    Move colormath helpers to the utils

--- pycoat/utils.py
+++ pycoat/utils.py
@@ -2,6 +2,9 @@
 
 from __future__ import division
 
+import numpy as np
+from colormath.color_objects import SpectralColor
+
 
 wavelength_conv = {
     'angstrom': 1e-10,
@@ -16,3 +19,21 @@ def rescale(x, to_units, from_units='nm'):
         raise ValueError('Unknown units')
 
     return x * scale
+
+
+def _prepare_rta(rta, prop):
+    indices = np.arange(rta.size)
+    mask = []
+    for (w, idx) in zip(rta.wavelength, indices):
+        if w % 10 == 0:
+            mask.append(idx)
+
+    filtered = rta.take(mask)
+    return {
+        'spec_%dnm' % w: x for (w, x) in (zip(filtered.wavelength, filtered[prop]))
+    }
+
+def rta_to_xyz(rta, prop, observer=2, illuminant='d65'):
+    spectrum_dict = _prepare_rta(rta, prop)
+    spc = SpectralColor(observer=observer, illuminant=illuminant, **spectrum_dict)
+    return spc.convert_to('xyz')
