1808c5e321cbd26b2b05847d6cb2610d2aee20d9
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2013-09-27 14:14:48 +0400

    Create test case for load() and dump() methods from hln module

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@
 import unittest
 
 from pycoat.core import Filter, Layer
+from pycoat.hln import load
 
 
 class TestCore(unittest.TestCase):
@@ -14,6 +15,18 @@ class TestCore(unittest.TestCase):
 
         self.assertAlmostEqual(f.physical_thickness(500), 170.7175, 3)
         self.assertAlmostEqual(f.physical_thickness(500, 'micron'), 0.1707, 3)
+
+
+class TestHln(unittest.TestCase):
+    def test_load(self):
+        f = load('SHL', {'S': 1.51, 'H': 2.0, 'L': 1.38})
+        self.assertEqual(f.substrate, 1.51)
+        self.assertEqual(f.layers[1].n, 1.38)
+        self.assertEqual(f.layers[1].nt, 1)
+
+    def test_dump(self):
+        f = load('SHLHL', {'S': 1.51, 'H': 2.0, 'L': 1.38})
+        self.assertEqual(f.dump(), 'S(HL)^2')
 
 
 if __name__ == '__main__':
